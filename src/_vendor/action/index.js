19366ad35e681665a355836d8feadae248ed10f9
akari0624 <akari0624@hotmail.com>
2017-11-23 18:08:28 +0800

    use formData and axios to upload files and input text

--- src/_vendor/action/index.js
+++ src/_vendor/action/index.js
@@ -48,13 +48,28 @@ export const doRevokeObjectURL = urlArr =>{
   }
 
 
-  export const uploadDataToServer = (mealArr) =>{
+  export const uploadDataToServer = (mealArr, img_fileArr) =>{
 
     console.log('ajax url',UPLOAD_SHOP_INIT_MEAL_LIST_URL);
 console.log(mealArr);
 
-   const response =  axios.post(UPLOAD_SHOP_INIT_MEAL_LIST_URL,{mealArr:mealArr});
+  const formDataObj = new FormData();
 
+  formDataObj.append('mealArr',JSON.stringify(mealArr));
+
+  for(let i=0;i<img_fileArr.length;i+=1) {
+
+    let currFile = img_fileArr[i];
+    formDataObj.append('menuImg_fileArr',currFile,currFile.name);
+
+  };
+
+
+    let config = {headers:{'Content-Type':'multipart/form-data'}};
+
+  // const response =  axios.post(UPLOAD_SHOP_INIT_MEAL_LIST_URL,{mealArr:mealArr});
+  const response =  axios.post(UPLOAD_SHOP_INIT_MEAL_LIST_URL, formDataObj, config);
+  
 
     return {
       type:WHEN_SHOP_INIT_MEAL_LIST_INSERT_DONE,
