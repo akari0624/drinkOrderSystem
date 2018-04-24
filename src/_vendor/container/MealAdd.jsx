96d23cfd84ede1a9e1e4c595d4012dc97ce56fd1
akari0624 <akari0624@hotmail.com>
2018-04-24 15:21:30 +0800

    do meal data verify when  addMeal

--- src/_vendor/container/MealAdd.jsx
+++ src/_vendor/container/MealAdd.jsx
@@ -1,31 +1,69 @@
 import React, { Component } from "react";
-import { Label, Input, Button } from "reactstrap";
+import { Label, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter, Container } from "reactstrap";
+import _cloneDeep from 'lodash.clonedeep';
 
 export default class MealAdd extends Component {
 
-  state = {
-   mealName:'',
-   unitPrice:''
-
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      mealInfo:{
+        mealName:'',
+        unitPrice:''
+         },
+        isInfoWindowOpen:false,
+        infoWindowMsg:''
+    }
   }
+
+
+
+verifyAddMeal = (mealObj) => {
+
+    if(mealObj.mealName === '' || mealObj.unitPrice === ''){
+      return false;
+    }
+    return true;
+}
 
 addMeal = () =>{
 
-  const copiedCurrentState = Object.assign({},this.state);
+  const copiedCurrentState = Object.assign({},this.state.mealInfo);
 
-  this.setState({
+  if(!this.verifyAddMeal(copiedCurrentState)){
+     
+     const currState = _cloneDeep(this.state);
+
+     currState.isInfoWindowOpen = true;
+     currState.infoWindowMsg = '餐點名稱與價格必須填寫！'
+     this.setState(currState);
+     return;
+  };
+
+  const currState = _cloneDeep(this.state);
+  currState.mealInfo = {
     mealName:'',
     unitPrice:''
-  });   
+  }
+  this.setState(currState);   
 
   this.props.addCallBack(copiedCurrentState);
  
 
 };
 
-mealNameOnChange =(e)=>{
+closeInfoWindow = () => {
+  const currState = _cloneDeep(this.state);
+  currState.isInfoWindowOpen = false;
+  currState.infoWindowMsg = '';
+  this.setState(currState);
+}
 
-  this.setState({mealName:e.target.value});
+mealNameOnChange =(e)=>{
+  const currState = _cloneDeep(this.state);
+  currState.mealInfo.mealName = e.target.value;
+  this.setState(currState);
 }
 
 unitPriceOnChange =(e)=>{
@@ -35,18 +73,31 @@ unitPriceOnChange =(e)=>{
   if(isNaN(parsedValue)){
     return;
   }
-  this.setState({unitPrice:parsedValue});
+  const currState = _cloneDeep(this.state);
+  currState.mealInfo.unitPrice = parsedValue;
+  this.setState(currState);
   
 }
 
   render() {
     return (
+      <Container>
+      <Modal isOpen={this.state.isInfoWindowOpen} toggle={this.closeInfoWindow}>
+          <ModalHeader toggle={this.closeInfoWindow}>
+              訊息：
+          </ModalHeader>
+          <ModalBody>
+          <div>
+            {this.state.infoWindowMsg}
+          </div>
+        </ModalBody>
+      </Modal>
       <div>
         <span>
           <Label for="inputMeal">餐名</Label>
-          <Input type="text" id="inputMeal" value={this.state.mealName} onChange={this.mealNameOnChange} />
+          <Input type="text" id="inputMeal" value={this.state.mealInfo.mealName} onChange={this.mealNameOnChange} />
           <Label for="inputPrice">單價</Label>
-          <Input type="text" id="inputPrice" placeholder="元" value={this.state.unitPrice} onChange={this.unitPriceOnChange} />
+          <Input type="text" id="inputPrice" placeholder="元" value={this.state.mealInfo.unitPrice} onChange={this.unitPriceOnChange} />
         </span>
         <div style={{
           marginTop:'20px'
@@ -55,6 +106,7 @@ unitPriceOnChange =(e)=>{
           <Button type="button" color="secondary" onClick={this.props.openPicDropUploadCallback}>上傳菜單圖片</Button>
         </div>
       </div>
+      </Container>
     );
   }
 
