b53eb6a1f931f2db4e91b979271d0c6b745e351d
akari0624 <akari0624@hotmail.com>
2018-05-15 22:42:59 +0800

    refactor this reducer, write default props more properly

--- src/reducers/JoinOrderDataREDUCER.js
+++ src/reducers/JoinOrderDataREDUCER.js
@@ -1,6 +1,28 @@
-import { JOIN_ORDER_FETCH_INIT_DATA_RESULT } from '../_joinOrder/type';
+import {JOIN_ORDER_FETCH_INIT_DATA_RESULT} from '../_joinOrder/type';
 
-export default (state = { errorMsg: '', joinOrderInfo: {} }, action) => {
+const defaultJoinOrderInfo = {
+
+    orderInfo: {
+        vendor_id: '',
+        coda: 0,
+        customMessage: '',
+        isEnd: false,
+        orders: [],
+        date: Date.now()
+    },
+    vendorInfo: {
+        vendor_name: '',
+        vendor_addreass: '',
+        vendor_tel: '',
+        meals: [],
+        menuImageString: []
+    }
+};
+
+export default(state = {
+    errorMsg: '',
+    joinOrderInfo: defaultJoinOrderInfo
+}, action) => {
     switch (action.type) {
     case JOIN_ORDER_FETCH_INIT_DATA_RESULT:
         return action.payload;
