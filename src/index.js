77a83b37ab301a00b6c4e168cda277483ed34ea4
akari0624 <akari0624@hotmail.com>
2018-10-12 15:46:54 +0800

    deploy Styled-Components theme props to every route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import ActionLoggerMiddleware from './middleware/ActionLogger';
 import reduxThunk from 'redux-thunk';
-
+import {ThemeProvider} from 'styled-components';
 
 import reducers from './reducers';
 import VendorMain from './_vendor/container/VendorAddMain';
@@ -14,69 +14,63 @@ import MakeOrder from './_makeOrder/container/MakeOrderMain';
 import MakingOrderConfirming from './_makeOrder/container/MakingOrderConfirming';
 import Making_Order_Result_Report_Page from './_makeOrder/container/Making_Order_Result_Report_Page';
 import JoinOrderMain from './_joinOrder/container/JoinOrderMain';
-import { FrontWebAppOrSubDirectoryBaseName }  from './static/url';
+import {FrontWebAppOrSubDirectoryBaseName} from './static/url';
 
 const LOCAL_STORAGE_KEY = 'customerServiceList';
 
-const createStoreWithMiddleware = applyMiddleware(
-    ActionLoggerMiddleware,
-    reduxThunk
-)(createStore);
+const theme = {
+    mobileOneColumnWidth: '750px',
+    padWidth: '980px'
+};
 
+const createStoreWithMiddleware = applyMiddleware(ActionLoggerMiddleware, reduxThunk)(createStore);
 
-if(process.env.NODE_ENV==='production'){
+if (process.env.NODE_ENV === 'production') {
 
-    const appStore = createStoreWithMiddleware(
-        reducers
-    );
-
+    const appStore = createStoreWithMiddleware(reducers);
 
     ReactDOM.render(
         <Provider store={appStore}>
-            <BrowserRouter basename={`/${FrontWebAppOrSubDirectoryBaseName}`}>
-                <div>
+            <ThemeProvider theme={theme}>
+                <BrowserRouter basename={`/${FrontWebAppOrSubDirectoryBaseName}`}>
+    
                     <Switch>
-                        <Route path="/vendor" component={VendorMain} />
-                        <Route path="/make_order" component={MakeOrder} />
-                        <Route path="/make_order_confirming/:index" component={MakingOrderConfirming} />
-                        <Route path="/making_order_result/" component={Making_Order_Result_Report_Page} />
-                        <Route path="/order/join/:orderId" component={JoinOrderMain} />
-                        <Route path="/" component={MainLandingPage} />
+                        <Route path="/vendor" component={VendorMain}/>
+                        <Route path="/make_order" component={MakeOrder}/>
+                        <Route path="/make_order_confirming/:index" component={MakingOrderConfirming}/>
+                        <Route
+                            path="/making_order_result/"
+                            component={Making_Order_Result_Report_Page}/>
+                        <Route path="/order/join/:orderId" component={JoinOrderMain}/>
+                        <Route path="/" component={MainLandingPage}/>
                     </Switch>
-                </div>
-            </BrowserRouter>
-        </Provider>,
-        document.querySelector('.container')
-    );
+    
+                </BrowserRouter>
+            </ThemeProvider>
+        </Provider>, document.querySelector('.container'));
 
-}else{
+} else {
 
-    const appStore = createStoreWithMiddleware(
-        reducers,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    );
-
-
+    const appStore = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
     ReactDOM.render(
         <Provider store={appStore}>
-            <BrowserRouter>
-                <div>
+            <ThemeProvider theme={theme}>
+                <BrowserRouter>
+    
                     <Switch>
-                        <Route path="/vendor" component={VendorMain} />
-                        <Route path="/make_order" component={MakeOrder} />
-                        <Route path="/make_order_confirming/:index" component={MakingOrderConfirming} />
-                        <Route path="/making_order_result/" component={Making_Order_Result_Report_Page} />
-                        <Route path="/order/join/:orderId" component={JoinOrderMain} />
-                        <Route path="/" component={MainLandingPage} />
+                        <Route path="/vendor" component={VendorMain}/>
+                        <Route path="/make_order" component={MakeOrder}/>
+                        <Route path="/make_order_confirming/:index" component={MakingOrderConfirming}/>
+                        <Route
+                            path="/making_order_result/"
+                            component={Making_Order_Result_Report_Page}/>
+                        <Route path="/order/join/:orderId" component={JoinOrderMain}/>
+                        <Route path="/" component={MainLandingPage}/>
                     </Switch>
-                </div>
-            </BrowserRouter>
-        </Provider>,
-        document.querySelector('.container')
-    );
+    
+                </BrowserRouter>
+            </ThemeProvider>
+        </Provider>, document.querySelector('.container'));
 
 }
-
-
-
