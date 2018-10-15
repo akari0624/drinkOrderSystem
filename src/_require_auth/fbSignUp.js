6a73d758cc940a31b412ccadcb87fceb1a232894
akari0624 <akari0624@hotmail.com>
2018-10-15 12:02:17 +0800

    decorate this page, a little bit...

--- src/_require_auth/fbSignUp.js
+++ src/_require_auth/fbSignUp.js
@@ -8,9 +8,31 @@ import axios from 'axios';
 import { fbSignUpOrLogInBackendRoute } from '../static/url';
 
 
+const MiddleDiv = Styled.div`
+
+  text-align:center;
+  width:40%;
+  background-color:#3b5998;
+  font-size:18px;
+  margin:30px auto;
+  padding:5px 30px 5px 30px;
+`;
+
+
+const FB_a_Tag = Styled.a`
+  color:#FFFFFF;
+  font-size:18px;
+
+  &:hover{
+    color:#FFFFFF;
+    text-decoration:none;
+  }
+`;
+
+
 /* 這邊一定要用<a href="..."> 不能用ajax，會有CORS問題 */
 const FBSignUp = props => (
-    <h3><a href={fbSignUpOrLogInBackendRoute}>登入FB以註冊帳號或登入</a></h3>
+    <MiddleDiv><FB_a_Tag href={fbSignUpOrLogInBackendRoute}>登入FB以註冊帳號或登入</FB_a_Tag></MiddleDiv>
 
 );
 
