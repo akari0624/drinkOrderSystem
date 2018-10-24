142e466977c88b7b1a4e8818e128104dfb9f230f
akari0624 <akari0624@hotmail.com>
2018-10-24 18:07:16 +0800

    use reactstrap Card

--- src/_main_landing_page/container/LandingPageMain.jsx
+++ src/_main_landing_page/container/LandingPageMain.jsx
@@ -1,81 +1,88 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Container, Alert } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import {
-    removeThoseFileArrAndObjectURLArr_but_also_returnObjectUrlArr_to_RevokeObjectURL,
-    doRevokeObjectURL,
-} from '../../_vendor/action';
+    Container,
+    Alert,
+    Card,
+    CardBody,
+    CardTitle,
+    CardText,
+    Button
+} from 'reactstrap';
 
-import {
-    getUserDataFromServerByUserOAuthIDAndSaveToReducer
-} from '../actions';
+import Styled from 'styled-components';
+import {removeThoseFileArrAndObjectURLArr_but_also_returnObjectUrlArr_to_RevokeObjectURL, doRevokeObjectURL} from '../../_vendor/action';
 
-import { 
-    removeGlobalInsertCompleteMessageOnCloseInsertResultAlert,
-    removeGlobalInsertCompleteMessageWhenUserExitMainLandingPage
-} from '../actions';
+import {getUserDataFromServerByUserOAuthIDAndSaveToReducer} from '../actions';
 
-import { grabFromCookie, deleteFromCookie, getSubFromJWT } from '../../util_func/util_func';
-import { JWT_KeyInCookie, isLoginLocalStorageKey, currRouteBeforeSignInKey } from '../../conf/keys';
+import {removeGlobalInsertCompleteMessageOnCloseInsertResultAlert, removeGlobalInsertCompleteMessageWhenUserExitMainLandingPage} from '../actions';
+
+import {grabFromCookie, deleteFromCookie, getSubFromJWT} from '../../util_func/util_func';
+import {JWT_KeyInCookie, isLoginLocalStorageKey, currRouteBeforeSignInKey} from '../../conf/keys';
+
+
+
+const PageTitleH1 = Styled.h1`
+  text-align:center;
+`;
+
 
 class LandingPageMain extends Component {
     constructor(props) {
         super(props);
 
-
         this.maybeYouAreRedirectByServerAfterDoingOAuth();
 
-        const isSholdRenderAlert = this.props.globalAppMessage.successMsg ? true : false;
+        const isShouldRenderAlert = this.props.globalAppMessage.successMsg
+            ? true
+            : false;
         this.state = {
-            shouldAlertOpen: isSholdRenderAlert,
+            shouldAlertOpen: isShouldRenderAlert
         };
 
+        this.shouldShowGlobalAppMessage = this
+            .shouldShowGlobalAppMessage
+            .bind(this);
 
-
-
-        this.shouldShowGlobalAppMessage = this.shouldShowGlobalAppMessage.bind(
-            this
-        );
-
-        this.closeAlertCB = this.closeAlertCB.bind(this);
+        this.closeAlertCB = this
+            .closeAlertCB
+            .bind(this);
     }
 
     closeAlertCB = () => {
-        this.setState({ shouldAlertOpen: false });
-        this.props.removeGlobalInsertCompleteMessageOnCloseInsertResultAlert();
+        this.setState({shouldAlertOpen: false});
+        this
+            .props
+            .removeGlobalInsertCompleteMessageOnCloseInsertResultAlert();
     };
-
-
-
 
     redirectCallbackOnGetUserDataSuccess = () => {
 
         /* 取到之後 就可以把cookie裡的登入後帶回來的jwt刪掉了 */
         deleteFromCookie(JWT_KeyInCookie);
 
-
         /* 如果驗證之前網址已經是去某頁的話，就跳過去 */
         this.resirectToOriginalPathBeforeSignIn_IfNeeded();
     }
-
 
     maybeYouAreRedirectByServerAfterDoingOAuth = () => {
 
         const authedJWT = grabFromCookie(JWT_KeyInCookie);
 
-        if(authedJWT){
+        if (authedJWT) {
 
-            window.localStorage.setItem(isLoginLocalStorageKey, authedJWT);
-            
+            window
+                .localStorage
+                .setItem(isLoginLocalStorageKey, authedJWT);
 
             const subInJWT = getSubFromJWT(authedJWT); //目前就是會員Facebook在這個app上的ID
 
             console.log('subInJWT', subInJWT);
-            this.props.getUserDataFromServerByUserOAuthIDAndSaveToReducer(subInJWT, this.redirectCallbackOnGetUserDataSuccess);
+            this
+                .props
+                .getUserDataFromServerByUserOAuthIDAndSaveToReducer(subInJWT, this.redirectCallbackOnGetUserDataSuccess);
 
-        
         }
 
     }
@@ -88,34 +95,66 @@ class LandingPageMain extends Component {
                 <Alert
                     isOpen={this.state.shouldAlertOpen}
                     color={'success'}
-                    toggle={this.closeAlertCB}
-                >
+                    toggle={this.closeAlertCB}>
                     {result.successMsg}
                 </Alert>
             );
         }
     }
 
+    toMakeOrder = (e) => {
+        this
+            .props
+            .history
+            .push('/make_order');
+
+    }
+
+    toCreateVendorData = (e) => {
+        this
+            .props
+            .history
+            .push('/vendor');
+
+    }
+
     render() {
         return (
             <Container>
                 {this.shouldShowGlobalAppMessage()}
-                <div>mainLanding Page</div>
-                <div>
-                    <Link to="/make_order">發起訂購</Link>
-                </div>
-                <div>
-                    <Link to="/vendor">新增店家</Link>
-                </div>
+                <PageTitleH1>訂購餐點 首頁</PageTitleH1>
+
+                <Card body inverse color="primary">
+                    <CardBody>
+                        <CardTitle style={{color:'#000000'}}>發起訂購</CardTitle>
+                        <CardText>從網友們建立的店家清單中挑選一間來訂購，不保證店家餐點內容的正確度</CardText>
+                        <Button onClick={this.toMakeOrder}>前往</Button>
+                    </CardBody>
+                </Card>
+
+                <Card body inverse color="info">
+                    <CardBody>
+                        <CardTitle style={{color:'#000000'}}>新增店家</CardTitle>
+                        <CardText>建立一間新的店家的資料和菜單資料，為了大家好，請資料盡量輸入正確</CardText>
+                        <Button onClick={this.toCreateVendorData}>前往</Button>
+                    </CardBody>
+                </Card>
             </Container>
         );
     }
 
-
     resirectToOriginalPathBeforeSignIn_IfNeeded = () => {
 
-        const pathBeforeDoOAuth = window.localStorage.getItem(currRouteBeforeSignInKey);
-        if(pathBeforeDoOAuth){
+        const pathBeforeDoOAuth = window
+            .localStorage
+            .getItem(currRouteBeforeSignInKey);
+        if (pathBeforeDoOAuth) {
+
+            // 移除快取起來的OAuth驗證前的route
+            window
+                .localStorage
+                .removeItem(currRouteBeforeSignInKey);
+
             this
                 .props
                 .history
@@ -126,43 +165,39 @@ class LandingPageMain extends Component {
 
     componentDidMount() {
         console.log('componentDidMount...onMainPage, remove cached image in browser objectURL');
-        this.props.removeThoseFileArrAndObjectURLArr_but_also_returnObjectUrlArr_to_RevokeObjectURL();
+        this
+            .props
+            .removeThoseFileArrAndObjectURLArr_but_also_returnObjectUrlArr_to_RevokeObjectURL();
     }
 
     UNSAFE_componentWillReceiveProps(nextProps) {
         if (this.props !== nextProps) {
             if (nextProps.shopImg_relatedData.srcToDelete.length > 0) {
-                nextProps.doRevokeObjectURL(
-                    nextProps.shopImg_relatedData.srcToDelete
-                );
+                nextProps.doRevokeObjectURL(nextProps.shopImg_relatedData.srcToDelete);
             }
         }
     }
 
     componentWillUnmount() {
-        this.props.removeGlobalInsertCompleteMessageWhenUserExitMainLandingPage();  
+        this
+            .props
+            .removeGlobalInsertCompleteMessageWhenUserExitMainLandingPage();
     }
 
 }
 
 function mapStateToProps(state) {
-    return {
-        globalAppMessage: state.mealListInsertResult,
-        shopImg_relatedData: state.imgPreviewSrcAndImgFile
-    };
+    return {globalAppMessage: state.mealListInsertResult, shopImg_relatedData: state.imgPreviewSrcAndImgFile};
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators(
-        {
-            removeThoseFileArrAndObjectURLArr_but_also_returnObjectUrlArr_to_RevokeObjectURL,
-            doRevokeObjectURL,
-            removeGlobalInsertCompleteMessageOnCloseInsertResultAlert,
-            removeGlobalInsertCompleteMessageWhenUserExitMainLandingPage,
-            getUserDataFromServerByUserOAuthIDAndSaveToReducer,
-        },
-        dispatch
-    );
+    return bindActionCreators({
+        removeThoseFileArrAndObjectURLArr_but_also_returnObjectUrlArr_to_RevokeObjectURL,
+        doRevokeObjectURL,
+        removeGlobalInsertCompleteMessageOnCloseInsertResultAlert,
+        removeGlobalInsertCompleteMessageWhenUserExitMainLandingPage,
+        getUserDataFromServerByUserOAuthIDAndSaveToReducer
+    }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(LandingPageMain);
