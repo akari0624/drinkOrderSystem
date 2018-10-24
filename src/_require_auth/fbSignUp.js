9e8bdb43302ffc82f555bbddd4656d91912d128e
akari0624 <akari0624@hotmail.com>
2018-10-24 18:03:41 +0800

    add service of term page link

--- src/_require_auth/fbSignUp.js
+++ src/_require_auth/fbSignUp.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Styled from 'styled-components';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
+import {bindActionCreators} from 'redux';
 import axios from 'axios';
 
-import { fbSignUpOrLogInBackendRoute } from '../static/url';
+import {fbSignUpOrLogInBackendRoute} from '../static/url';
 
 
-const MiddleDiv = Styled.div`
-
+const WrapperMain = Styled.main`
   text-align:center;
+`;
+
+const MiddleDiv = Styled.div `
   width:40%;
   background-color:#3b5998;
   font-size:18px;
@@ -18,8 +20,7 @@ const MiddleDiv = Styled.div`
   padding:5px 30px 5px 30px;
 `;
 
-
-const FB_a_Tag = Styled.a`
+const FB_a_Tag = Styled.a `
   color:#FFFFFF;
   font-size:18px;
 
@@ -29,32 +30,32 @@ const FB_a_Tag = Styled.a`
   }
 `;
 
+const ServiceTermDiv = Styled.div `
+
+  font-size:13px;
+  margin-top:-20px;
+  padding:5px 30px 5px 30px;
+`;
+
 
 /* 這邊一定要用<a href="..."> 不能用ajax，會有CORS問題 */
 const FBSignUp = props => (
-    <MiddleDiv><FB_a_Tag href={fbSignUpOrLogInBackendRoute}>登入FB以註冊帳號或登入</FB_a_Tag></MiddleDiv>
-
+    <WrapperMain>
+        <MiddleDiv>
+            <FB_a_Tag href={fbSignUpOrLogInBackendRoute}>登入FB以註冊帳號或登入</FB_a_Tag>
+        </MiddleDiv>
+        <ServiceTermDiv>註冊與登入之後即代表您同意本服務之<a href="/drink/termofservice">服務條款</a></ServiceTermDiv>
+    </WrapperMain>
 );
 
+FBSignUp.propTypes = {}
 
-FBSignUp.propTypes = {
-
-
-  }
-
-
-function mapStateToProps(state){
-  return {}
+function mapStateToProps(state) {
+    return {}
 }
 
-
-
-
-function mapDispatchToProps(dispatch){
-return bindActionCreators({
-},
-dispatch)
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({}, dispatch)
 }
-
 
 export default connect(null, mapDispatchToProps)(FBSignUp)
