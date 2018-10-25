ef96fb08fce184f6d64039a0c20c31ee42e5811a
akari0624 <akari0624@hotmail.com>
2018-10-25 19:13:02 +0800

    remove debug mongo document _id

--- src/_joinOrder/component/Others_order_ListArea.js
+++ src/_joinOrder/component/Others_order_ListArea.js
@@ -11,7 +11,7 @@ const OthersOrder_ListArea = props => (
             .othersOrder
             .map(o => (
                 <div key={o._id}>
-                    {`${o.orderer_name}  ${o.ordered_mealName}  數量:${o.quantity} 小計${o.subTotal}元  訂購餐點後輸入mongo的_ID  ${o._id}`}
+                    {`${o.orderer_name}  ${o.ordered_mealName}  數量:${o.quantity} 小計${o.subTotal}元`}
                 </div>
             ))}
     </div>
